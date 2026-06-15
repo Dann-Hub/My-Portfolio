9dd3795fb4de6b8138663d7ed96c372798ff1723
shadownet <danieloseiboateng5@gmail.com>
2026-06-15 21:19:00 +0000

    Update on About page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
-import { Intro } from "@/components/intro"
 import { Footer } from "@/components/footer"
 import About from "@/components/about";
-import Skills from "@/components/skills";
-import Stats from "@/components/stats";
-import Experience from "@/components/experience";
-import Reveal from "@/components/reveal";
+
 
 
 export default function Page() {
@@ -16,12 +12,7 @@ export default function Page() {
                 <Header />
                 <Hero />
             </div>
-            {/*<Intro />*/}
             <About />
-
-            {/*<Stats />*/}
-            {/*<Skills />*/}
-            {/*<Experience />*/}
             <Footer />
         </main>
     )
